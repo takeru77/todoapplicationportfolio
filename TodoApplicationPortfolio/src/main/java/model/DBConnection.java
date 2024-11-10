a76837a0cf4144eaf5ac26285a7d63ef40075f62
takec <takec@DESKTOP-A73N9QU>
2024-11-10 22:55:38 +0900

    本番用のデータベースURLに更新。

--- TodoApplicationPortfolio/src/main/java/model/DBConnection.java
+++ TodoApplicationPortfolio/src/main/java/model/DBConnection.java
@@ -9,9 +9,9 @@ public class DBConnection {
 	public static Connection getConnection() throws SQLException, ClassNotFoundException {
 		// データベースのURL
 		// 本番用
-//		final String URL = "jdbc:postgresql://153.126.161.113:5432/useraccount";
+		final String URL = "jdbc:postgresql://153.126.161.113:5432/useraccount";
 		// ローカル用
-		final String URL = "jdbc:postgresql://172.20.10.4:5432/UserAccount";
+//		final String URL = "jdbc:postgresql://172.20.10.4:5432/UserAccount";
 		// データベースにアクセスするユーザー
 		final String USER = "tda";
 		// データベースのパスワード
